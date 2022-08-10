b3219efae397251eb05751e88c6c646cefbefa60
Débora Ferreira <deboraleticiaferreiramaciel@gmail.com>
2022-08-10 12:17:09 -0300

    home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -19,7 +19,7 @@ export default function Home() {
     // setItemCart(localStorage.getItem("cart").split());
   }
 
-  const addObjectToArray = (obj) => {
+  const OnAddToCart = (obj) => {
     var name = itemsLR.find((x) => x.id === obj).name;
     var message = name + " added to cart!";
     setItemCart((current) => [...current, obj]);
@@ -98,36 +98,54 @@ export default function Home() {
               Lord of the Rings
             </b>
           </h1>
-          <div className="w-full grid grid-cols-3">
+          <div className="w-full grid grid-cols-4 gap-9">
             {itemsLR.map((item) => (
-              <div className="card bg-white m-6 p-6 text-center rounded-lg shadow-2xl">
-                <img
-                  className="rounded-lg border-2 border-slate-300"
-                  src={require(`./../../assets/LR_Swords/${item.imageUrl}`)}
-                  alt={item.imageUrl}
-                />
-
-                <Link to={`item/${item.id}`}>
-                  <div className="m-2 text-center h-14">{item.name}
-                  </div></Link>
-                <p className="text-sm font-medium">${item.price}</p>
-
-                <button
-                  onClick={() => addObjectToArray(item.id)}
-                  type="button"
-                  className="text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center mr-2 dark:bg-orange-600 dark:hover:bg-orange-700 dark:focus:ring-orange-800"
-                >
-                  <svg
-                    aria-hidden="true"
-                    className="mr-2 -ml-1 w-5 h-5"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path>
-                  </svg>
-                  Add to card
-                </button>
+              <div >
+                <div >
+                  <div>
+                    <div class="max-w-md w-full bg-slate-50 shadow-lg rounded-xl p-6">
+                      <div class="flex flex-col">
+                        <div class="">
+                          <div class="relative h-62 w-full mb-3 mr-2">
+                            <div class="absolute flex flex-col top-0 right-0 p-3">
+                              <button class="transition ease-in duration-300 bg-white hover:text-orange-500 shadow hover:shadow-md text-gray-500 rounded-full w-8 h-8 text-center p-1"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
+                              </svg></button>
+                            </div>
+                            <img src={require(`./../../assets/LR_Swords/${item.imageUrl}`)} alt={item.imageUrl} class=" w-full object-fill border border-gray-300 rounded-2xl" />
+                          </div>
+                          <div class="flex-auto justify-evenly">
+                            <div class="flex flex-wrap ">
+                              <div class="w-full flex-none text-sm flex items-center text-gray-600">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-yellow-600 mr-1" viewBox="0 0 20 20" fill="currentColor">
+                                  <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+                                </svg>
+                                <span class="text-gray-400 whitespace-nowrap mr-3">{item.star}</span><span class="mr-2 text-gray-400"></span>
+                              </div>
+                              <div class="flex items-center w-full justify-between min-w-0 h-16">
+                                <Link to={`item/${item.id}`}><h2 class="text-base mr-auto cursor-pointer whitespace-normal break-words text-gray-800 font-medium hover:text-orange-800 truncate ">{item.name}</h2></Link>
+                              </div>
+                            </div>
+                            <div class="text-xl text-black font-semibold mt-2 mb-4 ml-2">${item.price}</div>
+                            <div class="flex space-x-2 text-sm font-medium justify-start">
+                              <button onClick={() => OnAddToCart(item.id)} class="transition ease-in duration-300 inline-flex items-center text-sm font-medium mb-2 md:mb-0 bg-orange-800 px-5 py-2 hover:shadow-lg tracking-wider text-white rounded-lg hover:bg-orange-800 ">
+                                <span>Add Cart</span>
+                              </button>
+                              <Link to={`item/${item.id}`}>
+                                <button class="transition ease-in duration-300 bg-orange-700 hover:bg-orange-800 border hover:border-orange-800 border-orange-600 hover:text-white  hover:shadow-lg text-gray-100 rounded-full w-9 h-9 text-center p-2">
+                                  <svg xmlns="http://www.w3.org/2000/svg" class="" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                                  </svg>
+                                </button>
+                              </Link>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
             ))}
           </div>
@@ -342,6 +360,7 @@ var itemsLR = [
     price: 98.22,
     color: "Salmon",
     quantity: 1,
+    star: 4.5
   },
   {
     id: "2",
@@ -350,6 +369,7 @@ var itemsLR = [
     price: 104.02,
     color: "Salmon",
     quantity: 1,
+    star: 4.5
   },
   {
     id: "3",
@@ -358,6 +378,7 @@ var itemsLR = [
     price: 63.77,
     color: "Salmon",
     quantity: 1,
+    star: 4.5
   },
   {
     id: "4",
@@ -366,6 +387,7 @@ var itemsLR = [
     price: 274.02,
     color: "Salmon",
     quantity: 1,
+    star: 4.5
   },
   {
     id: "5",
@@ -374,6 +396,7 @@ var itemsLR = [
     price: 78.50,
     color: "Salmon",
     quantity: 1,
+    star: 4.5
   },
   {
     id: "6",
@@ -382,6 +405,7 @@ var itemsLR = [
     price: 85.20,
     color: "Salmon",
     quantity: 1,
+    star: 4.5
   },
   {
     id: "7",
@@ -390,6 +414,7 @@ var itemsLR = [
     price: 91.60,
     color: "Salmon",
     quantity: 1,
+    star: 4.5
   },
   {
     id: "8",
@@ -398,6 +423,7 @@ var itemsLR = [
     price: 47.18,
     color: "Salmon",
     quantity: 1,
+    star: 4.5
   },
   {
     id: "9",
@@ -406,6 +432,7 @@ var itemsLR = [
     price: 24.01,
     color: "Salmon",
     quantity: 1,
+    star: 4.5
   },
   {
     id: "10",
@@ -414,6 +441,7 @@ var itemsLR = [
     price: 99.90,
     color: "Salmon",
     quantity: 1,
+    star: 4.5
   },
   {
     id: "11",
@@ -422,6 +450,7 @@ var itemsLR = [
     price: 214.02,
     color: "Salmon",
     quantity: 1,
+    star: 4.5
   },
   {
     id: "12",
@@ -430,5 +459,6 @@ var itemsLR = [
     price: 324.02,
     color: "Salmon",
     quantity: 1,
+    star: 4.5
   },
 ];
